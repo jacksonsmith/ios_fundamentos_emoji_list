dfe0f6aff1e47557e3bbd1c21cb0595e1a05f094
Jackson <jackson.smith@sanar.com>
2022-07-06 18:24:36 -0300

    initial commit

---
+++ AppList/TableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TableViewController.swift
+//  AppList
+//
+//  Created by Jackson on 06/07/22.
+//
+
+import Foundation
+import UIKit
+
+class TableViewController : UITableViewController {
+    
+    let emotes: [String] = ["😆", "😂", "😁", "😊", "😉", "😚", "😎", "🐶", "😇", "😝"]
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("cell positon: \(indexPath.row) taped")
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        
+        cell.backgroundColor = .white
+        cell.textLabel?.text = "\(indexPath.row): \(emotes[indexPath.row])"
+        cell.textLabel?.textAlignment = .center
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emotes.count
+    }
+}
+
